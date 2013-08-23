c7823b700223a624d85a632ff92c2392408770c6
Agam Rafaeli <agam.rafaeli@gmail.com>
2013-08-23 23:54:50 +0300

    Dynamic loading of ShowEvent page

--- kitchencrashers/main/urls.py
+++ kitchencrashers/main/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^$', 'kitchencrashers.main.views.home', name='home'),
     url(r'^createEvent$', 'kitchencrashers.main.views.createEvent', name='createEvent'),
-    url(r'^showEvent$', 'kitchencrashers.main.views.showEvent', name='showEvent')
+    url(r'^showEvent/(?P<eventID>\d+)$','kitchencrashers.main.views.showEvent', name='showEvent')
 )
--- kitchencrashers/main/views.py
+++ kitchencrashers/main/views.py
@@ -1,5 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
+from models import Event
+
+COOK = 'Cook'
+CLEANER = 'Cleaner'
 
 def home(request):
 	return render(request,"index.html")
@@ -7,5 +11,9 @@ def home(request):
 def createEvent(request):
 	return render(request,"CreateEvent.html")
 
-def showEvent(request):
-	return render(request,"ShowEvent.html")
+def showEvent(request,eventID):
+	event = Event.objects.get(pk=eventID)
+	event.cooks = event.participants.filter(rsvp=COOK).count()
+	event.cleaners = event.participants.filter(rsvp=CLEANER).count()
+	event.per_person = event.budget / (event.participants.count() + 1)
+	return render(request,"ShowEvent.html",{'event':event})
