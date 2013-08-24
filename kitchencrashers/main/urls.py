a3d2d8fd9d59bde30d755c67eab64350c0ab9a95
Gal Hochberg <gal@hiredscore.com>
2013-08-24 18:11:19 +0300

    Added a search interface with dynamic query building.

--- kitchencrashers/main/urls.py
+++ kitchencrashers/main/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'kitchencrashers.main.views.home', name='home'),
+    url(r'^search$', 'kitchencrashers.main.views.search', name='search'),
 )
